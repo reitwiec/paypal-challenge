4a5cc6f563aa55b5211332b95351019377883728
reitwiec <reitwiec@gmail.com>
2020-05-03 00:16:09 +0530

    Updates

---
+++ precache-manifest.abe69dbc47df808f54a6dd69a583735e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9962e2e71605c4ed8fab4fd1f7def8",
+    "url": "/paypal-challenge/index.html"
+  },
+  {
+    "revision": "6cf08c58d1d9a97dfc84",
+    "url": "/paypal-challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d954f0364c3c361c7030",
+    "url": "/paypal-challenge/static/js/2.a050de45.chunk.js"
+  },
+  {
+    "revision": "0472107b6f246c1e2ca64f14170aecd1",
+    "url": "/paypal-challenge/static/js/2.a050de45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf08c58d1d9a97dfc84",
+    "url": "/paypal-challenge/static/js/main.73ba327c.chunk.js"
+  },
+  {
+    "revision": "ed4574df9d064a3a59ce",
+    "url": "/paypal-challenge/static/js/runtime-main.548c11c2.js"
+  },
+  {
+    "revision": "9ff7465553490fe5c5f872d138110a71",
+    "url": "/paypal-challenge/static/media/data.9ff74655.csv"
+  }
+]);
